2a7edba88274229fbd344cdd20648aceea87d60d
madhug-nadig <madhug.nadig@gmail.com>
2016-03-22 20:08:21 +0530

    Add week 2 files

---
+++ Week 2/dishes.js
@@ -0,0 +1,59 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+require('mongoose-currency').loadType(mongoose);
+var Currency = mongoose.Types.Currency;
+
+var commentSchema = new Schema({
+    rating:  {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment:  {
+        type: String,
+        required: true
+    },
+    author:  {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+var dishSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        required: true,
+        default: ""
+    },
+    price: {
+        type: Currency,
+        required: true
+    },
+    description: {
+        type: String,
+    },   
+    comments:[commentSchema]
+}, {
+    timestamps: true
+});
+
+var Dishes = mongoose.model('Dish', dishSchema);
+
+module.exports = Dishes;
